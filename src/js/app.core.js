9afe6852ffab2da65e0450038b465013895ba991
Alexeii Yatsenko <alexeii.yatsenko@gmail.com>
2020-02-21 14:53:56 +0200

    added sync for seconds

--- src/js/app.core.js
+++ src/js/app.core.js
@@ -1,39 +1,54 @@
-const numbers = [
-    { path: 'src/img/0.png', alt: 'Number zero' },
-    { path: 'src/img/1.png', alt: 'Number one' },
-    { path: 'src/img/2.png', alt: 'Number two' },
-    { path: 'src/img/3.png', alt: 'Number three' },
-    { path: 'src/img/4.png', alt: 'Number four' },
-    { path: 'src/img/5.png', alt: 'Number five' },
-    { path: 'src/img/6.png', alt: 'Number six' },
-    { path: 'src/img/7.png', alt: 'Number seven' },
-    { path: 'src/img/8.png', alt: 'Number eigth' },
-    { path: 'src/img/9.png', alt: 'Number nine' }
-]
-
 function clockInit() {
     const root = document.getElementById('clock');
+    const numbers = [
+        { path: 'src/img/0.png', alt: 'Number zero' },
+        { path: 'src/img/1.png', alt: 'Number one' },
+        { path: 'src/img/2.png', alt: 'Number two' },
+        { path: 'src/img/3.png', alt: 'Number three' },
+        { path: 'src/img/4.png', alt: 'Number four' },
+        { path: 'src/img/5.png', alt: 'Number five' },
+        { path: 'src/img/6.png', alt: 'Number six' },
+        { path: 'src/img/7.png', alt: 'Number seven' },
+        { path: 'src/img/8.png', alt: 'Number eigth' },
+        { path: 'src/img/9.png', alt: 'Number nine' }
+    ]
 
-    createBasement(root);
+    createBasement(root, numbers);
+    synchronize(numbers);
 }
 
-function createBasement(parent) {
-    createSection(parent, 'hours');
-    createSection(parent, 'minutes');
-    createSection(parent, 'seconds');
+function createBasement(parent, imagesCollection) {
+    createSection(parent, 'hours', imagesCollection);
+    createSection(parent, 'minutes', imagesCollection);
+    createSection(parent, 'seconds', imagesCollection);
 }
 
-function createSection(parent, sectionName) {
+function createSection(parent, sectionId, imagesCollection) {
     const section = createAndAppend(parent, 'div');
-    section.dataset.section = sectionName;
+    section.setAttribute('id', sectionId);
 
-    createSectionUnit(section, '__dozens');
-    createSectionUnit(section, '__units');
+    createSectionUnit(section, `${sectionId}__dozens`, imagesCollection);
+    createSectionUnit(section, `${sectionId}__units`, imagesCollection);
 }
 
-function createSectionUnit(parent, suffix) {
+function createSectionUnit(parent, id, imagesCollection) {
     const unit = createAndAppend(parent, 'img');
-    unit.dataset.section = parent.dataset.section + suffix;
-    unit.setAttribute('src', numbers[0].path);
-    unit.setAttribute('alt', numbers[0].alt);
+    unit.setAttribute('id', id);
+    unit.setAttribute('src', imagesCollection[0].path);
+    unit.setAttribute('alt', imagesCollection[0].alt);
 }
+
+function synchronize(imagesCollection) {
+    setItemImage('seconds__units', imagesCollection, getTimeItem('seconds', 'units'));
+    setItemImage('seconds__dozens', imagesCollection, getTimeItem('seconds', 'dozens'));
+
+    setInterval(() => {
+        setItemImage('seconds__units', imagesCollection, getTimeItem('seconds', 'units'));
+    }, 1000);
+
+    setInterval(() => {
+        setItemImage('seconds__dozens', imagesCollection, getTimeItem('seconds', 'dozens'));
+    }, 10000);
+}
+
+
--- src/js/utils.js
+++ src/js/utils.js
@@ -9,3 +9,47 @@ function createAndAppend(parent, tagName, content) {
 
     return parent.lastElementChild;
 }
+
+function setItemImage(itemId, imagesCollection, number) {
+    const img = document.getElementById(itemId);
+    img.setAttribute('src', imagesCollection[number].path);
+    img.setAttribute('alt', imagesCollection[number].alt);
+}
+
+function getTimeItem(section, measurement) {
+    switch (section) {
+        case 'hours':
+            return getRealHours(measurement);
+        case 'minutes':
+            return getRealMinutes(measurement);
+        case 'seconds':
+            return getRealSeconds(measurement);
+    }
+}
+
+function getRealHours(measurement) {
+    switch (measurement) {
+        case 'units':
+            return Math.floor(new Date().getHours() % 10);
+        case 'dozens':
+            return Math.floor(new Date().getHours() / 10);
+    }
+}
+
+function getRealMinutes(measurement) {
+    switch (measurement) {
+        case 'units':
+            return Math.floor(new Date().getMinutes() % 10);
+        case 'dozens':
+            return Math.floor(new Date().getMinutes() / 10);
+    }
+}
+
+function getRealSeconds(measurement) {
+    switch (measurement) {
+        case 'units':
+            return Math.floor(new Date().getSeconds() % 10);
+        case 'dozens':
+            return Math.floor(new Date().getSeconds() / 10);
+    }
+}
